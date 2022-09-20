f9c5a833a5e8334b7107de9074de1f4831d4d025
Mohammed Ammar Ammar <m.ammar4web@gmail.com>
2022-09-20 21:26:18 +0300

    JSX: function: return() | class: render() { return(); }

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import React from "react";
+import "./App.css";
+// 
+import Test from "./Test";
+
+// function App() {return ();} 
+// use Function rether than class when ever you can
 
 function App() {
   return (
     <div className="App">
+      <h1>Hello. I'm Ammar</h1>
+      <Test />
     </div>
   );
 }
---
+++ src/Test.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+// class Test extends React.Component { render() {return ();} }
+// <React.Fragment></React.Fragment> }
+
+class Test extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h2>Test: class | render() | return(...)</h2>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Test;
