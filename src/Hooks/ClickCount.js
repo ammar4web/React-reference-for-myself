47a20f8212a6c8fc0d00b3f02319d2f5b075392d
Mohammed Ammar Ammar <m.ammar4web@gmail.com>
2022-09-21 08:34:34 +0300

    Hooks: useEffect =~ componentDidMount() and componentDidUpdate()

--- src/Hooks/ClickCount.js
+++ src/Hooks/ClickCount.js
@@ -1,16 +1,35 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function ClickCount() {
-  // 
+  //
   // hooks | useState
   const [counter, setCounter] = useState(0);
-  // 
+  //
+
+  //
+  // useEffect
+  // سيعمل قبل الريندر
+  // componentDidMount() مثل الدالة
+  useEffect(() => {
+    alert("تم تصيير هذا المكون");
+  }, []);
+  //
+
+  //
+  // useEffect
+  // componentDidUpdate() مثل الدالة
+  useEffect(() => {
+    if (counter === 5) {
+      alert("تم النقر 5 مرات");
+    }
+  }, [counter]);
+  //
 
   return (
     <React.Fragment>
       <hr />
       <h4>ClickCount Components</h4>
-      <p>لقد نقرتني { counter } مرات</p>
+      <p>لقد نقرتني {counter} مرات</p>
       <button onClick={() => setCounter(counter + 1)}>انقر الزر</button>
     </React.Fragment>
   );
