5623f7fde2b88fc1a30b22d409f838ed369148ce
Mohammed Ammar Ammar <m.ammar4web@gmail.com>
2022-09-24 08:05:03 +0300

    Style in JSX

--- src/if/One.js
+++ src/if/One.js
@@ -12,20 +12,30 @@ function One() {
   const [onLine, setOnLine] = useState(true);
   const [userType, setUerType] = useState('Admin');
   
-  const hsoubProducts = ['a', 'b', 'c'];
-  const print = hsoubProducts.map(item => <li key={item + "1"}>{item}</li>)
+  const hsoubProducts = ['a', 'b', 'c', 'a'];
+  const print = hsoubProducts.map((item, index) => <li key={index}>{item}</li>)
 
   let userStatus = <OffLine />;
   if (onLine) {
     userStatus = <OnLine />
   }
 
+  const h1Styl = {
+    textAlign: "left",
+    color: "red"
+  }
+
+  const [isBlue, setBlue] = useState(false);
+
+  if (isBlue) {
+    h1Styl['color'] = 'blue';
+  }
   return (
     <React.Fragment>
-      <h1>عنصر مصير دائما</h1> 
+      <h1 style={h1Styl}>عنصر مصير دائما</h1> 
       {userStatus}
       {window.outerWidth > 800 && <h2>هذا التطبيق مفتوح من خلال الحاسب</h2>}
-      <h3>{userType === 'Admin' ? 'Admin': 'User'}</h3>
+      <h3 style={h1Styl} onClick={() => setBlue(!isBlue) }>{userType === 'Admin' ? 'Admin': 'User'}</h3>
       {/*  */}
       <ul>
         {print}
